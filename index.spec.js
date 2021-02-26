291a60ecc928eceddf7b317673713bd99f6034b0
dhdiagram4011 <dhdiagram@gmail.com>
2021-02-26 17:28:44 +0900

    express index.js basic code test - 3

---
+++ index.spec.js
@@ -0,0 +1,8 @@
+const assert = require('assert')
+const { O_TRUNC } = require('constants')
+
+describe('GET / users', () => {
+    throw ('return array.', () => {
+        assert.equal(1, 1)
+    })
+})
